9868731340207952ea31cbdc76f2c2a444e07496
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2015-05-26 18:35:44 +0300

    Update theme and local-images for better compatibility with Word

--- plugins/local-images/index.js
+++ plugins/local-images/index.js
@@ -3,23 +3,28 @@ var through2 = require("through2"),
 	File = require("vinyl");
 
 module.exports = function(options) {
+	var no = 1;
 	return through2.obj(function(file, enc, done) {
 		if (!file.$) return done(null, file);
 
 		var $ = file.$;
 
 		$("img").each(function(i, el) {
+			no = no + 1;
 			var $el = $(el);
 			var src = $el.attr("src");
+			var alt = $el.attr("alt") || "الشكل رقم (" + no + ")";
 
 			var img = new File({
 				contents: fs.readFileSync(src),
 				path: src
 			});
 
-			var no = src.substr(0, src.length - 4);
+			// var no = src.substr(0, src.length - 4);
 
-			$el.replaceWith("<figure no='" + no + "'><img src='" + src + "'/></figure>")
+			$el.parent("p").addClass("figure-container").prev("p").addClass("figure-p");
+
+			$el.after("<br>" + alt);
 
 			this.push(img);
 
